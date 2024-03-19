a9c4ed2f6136d74aaa44411b20832de62046f18c
Mil-m <mansurova.m94@gmail.com>
2024-03-19 13:59:45 +0000

    abc-292 updates for not-found

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -12,5 +12,12 @@ export const routes: Routes = [
         children: [
             { path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule) }
         ]
+    },
+    {
+        path: '',
+        children: [
+            { path: '404', loadChildren: () => import('./not-found/not-found.module').then(m => m.NotFoundModule) },
+            { path: '**', loadChildren: () => import('./not-found/not-found.module').then(m => m.NotFoundModule) }
+        ]
     }
 ];
--- src/app/dashboard/component/dashboard.component.ts
+++ src/app/dashboard/component/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { OnInit, Component, ViewChild} from '@angular/core';
+import { OnInit, Component, ViewChild } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
 import { Sample, samples } from '../model/dashboard.model';
